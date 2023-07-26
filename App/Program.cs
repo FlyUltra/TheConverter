12324c73f8ff289bd92138275ae66c9984dfd95d
Fly <67563726+FlyUltra@users.noreply.github.com>
2023-07-26 22:16:15 +0200

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Program {
+    
+    static void Main(string[] args) {
+        
+        Console.Write("Teplota v Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = celsius * 9 / 5 + 32;
+        
+        Console.WriteLine("Teplota "+celsius+" °C se rovná "+fahrenheit+" °F.");
+    }
+    
+}
